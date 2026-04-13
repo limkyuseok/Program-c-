56a454720370ad97623ecb6712b4387646cc56d7
limkyuseok <lgkk50025@gmail.com>
2026-04-13 21:42:24 +0900

    (2026. 04. 13 박스 & 언박싱)

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Program_C__
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 박싱
+            // 값 타입을 참조 타입으로 변환하여 관리되는 힙 영역에
+            // 새로운 객체를 만들고 복사하는 과정입니다.
+
+            //int count = 1;
+
+            //object clone = count;
+
+            //Console.WriteLine("count : " + count);
+            //Console.WriteLine("clone : " + clone);
+
+            #endregion
+
+            #region 언박싱
+            // 관리되는 힙에 있는 박싱되어 있는 객체에서 값을 꺼내
+            // 값 타입으로 복사하는 과정입니다.
+
+            //long experience = 3000;
+
+            //object address = experience;
+
+            //long data = (long)address;
+
+            //Console.WriteLine("experience : " + experience);
+            //Console.WriteLine("address : " + address);
+            //Console.WriteLine("data : " + data);
+
+            #endregion
+        }
+    }
+}
