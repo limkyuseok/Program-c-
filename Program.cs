77e0ed04e870f58581f8831e008c5b6a9b32df07
limkyuseok <lgkk50025@gmail.com>
2026-04-14 21:44:53 +0900

    (2026. 04. 14 가비지 컬렉션)

--- Program.cs
+++ Program.cs
@@ -1,37 +1,56 @@
-﻿namespace Program_C__
-{
+﻿
+
+using System.Runtime.CompilerServices;
+
+public class Circle
+    {
+    public int x;
+    public int y;
+    public float radius;
+
+    public Circle()
+        {
+            Console.WriteLine("Created Circle");
+        }
+}
+
     internal class Program
     {
-        static void Main(string[] args)
+         static void Collide(Circle origin, Circle other)
         {
-            #region 박싱
-            // 값 타입을 참조 타입으로 변환하여 관리되는 힙 영역에
-            // 새로운 객체를 만들고 복사하는 과정입니다.
+        float deltaX = origin.x - other.x;
+        float deltaY = origin.y - other.y;
 
-            //int count = 1;
+        float radius = (origin.radius + other.radius) * (origin.radius + other.radius);
 
-            //object clone = count;
+        if (deltaX * deltaY + deltaY * deltaX <= radius)
+        {
 
-            //Console.WriteLine("count : " + count);
-            //Console.WriteLine("clone : " + clone);
-
-            #endregion
-
-            #region 언박싱
-            // 관리되는 힙에 있는 박싱되어 있는 객체에서 값을 꺼내
-            // 값 타입으로 복사하는 과정입니다.
-
-            //long experience = 3000;
-
-            //object address = experience;
-
-            //long data = (long)address;
-
-            //Console.WriteLine("experience : " + experience);
-            //Console.WriteLine("address : " + address);
-            //Console.WriteLine("data : " + data);
-
-            #endregion
         }
+        else
+        {
+
+        }
+    }
+    static void Main(string[] args)
+        {
+        #region 매개 변수 한정자
+        // 인수가 함수에 전달되는 방식과 사용 규칙을 제어하는 한정자입니다.
+
+        Circle circle = new Circle();
+
+        circle.x = 5;
+        circle.y = 5;
+        circle.radius = 1.0f;
+
+        Circle quadrant = new Circle();
+
+        quadrant.x = 1;
+        quadrant.y = 2;
+        quadrant.radius = 1.0f;
+
+        Collide(circle, quadrant);
+
+        #endregion
     }
 }
