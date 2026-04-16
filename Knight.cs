5dab9c42dbdbb42032093693f6b68916e68b0616
limkyuseok <lgkk50025@gmail.com>
2026-04-16 21:44:06 +0900

    (2026. 04. 16 추상 클래스)

---
+++ Knight.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Program
+{
+    internal class Knight : Unit
+    {
+
+        public override void Attack()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Unit.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Program
+{
+    internal abstract class Unit
+    {
+        protected int health;
+        protected int defense;
+
+        public abstract void Attack();
+    }
+}
