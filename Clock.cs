27c8503fbd6607b190d4adfd5e9ad44033ab2d17
limkyuseok <lgkk50025@gmail.com>
2026-04-21 21:44:28 +0900

    (2026. 04. 21 프로퍼티)

---
+++ Clock.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Program
+{
+    internal class Clock : ISelectable
+    {
+        public void Select()
+        {
+            
+        }
+    }
+}
---
+++ ISelectable.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Program
+{
+    internal interface ISelectable
+    {
+        public void Select();
+    }
+}
