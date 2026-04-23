9626a93b459ec6840bc2c20bf7ccde35703110e3
limkyuseok <lgkk50025@gmail.com>
2026-04-23 21:43:37 +0900

    (2026. 04. 23 일반화)

---
+++ Factory.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Text;
+
+namespace Program
+{
+    internal class Factory<T> where T : new()
+    {
+        private T[] objects;
+        private int count;
+
+        public Factory(int size = 5)
+        {
+            objects = new T[size];
+        }
+
+        public T Instantiate()
+        {
+            if(count >= objects.Length)
+            {
+                Console.WriteLine("No more can be created");
+
+                return default;
+            }
+
+            T clone = new T();
+
+            objects[count] = clone;
+
+            return objects[count++];
+        }
+    }
+}
---
+++ Obstacle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace Program
+{
+    internal class Obstacle
+    {
+        private int x;
+        private int y;
+
+        public void Translate(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+
+            Console.WriteLine("x : " + x);
+            Console.WriteLine("y : " + y);
+        }
+
+    }
+}
