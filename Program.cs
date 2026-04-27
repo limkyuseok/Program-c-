20081e0c10323b3de24ab956574dff36a546a16d
limkyuseok <lgkk50025@gmail.com>
2026-04-27 11:47:21 +0900

    (2026. 04. 26 컬렉션)

--- Program.cs
+++ Program.cs
@@ -5,21 +5,96 @@ namespace Program
     {
         static void Main(string[] args)
         {
-            #region 일반화
-            // 데이터 형식에 의존하지 않고, 하나의 값이 여러 다른 데이터 형식을
-            // 가질 수 있는 기술에 중점을 두어 재사용성을 높일 수 있는 기능입니다.
+            #region 컬렉션
+            // 데이터를 저장하고, 추가하고, 삭제하며 순회하는 기능까지 포함한
+            // 데이터 관리 구조입니다.
 
-            Factory<Obstacle> factory = new Factory<Obstacle>(5);
+            #region List
 
+            //List<int> list = new List<int>();
 
-            for(int i = 0; i < 5; i++)
-            {
-               Obstacle obstacle = factory.Instantiate();
+            //list.Capacity = 10;
 
-                obstacle.Translate(i, i);
-            }
+            //list.Add(10);
+            //list.Add(20);
+            //list.Add(30);
+            //list.Add(40);
+            //list.Add(50);
+
+            //list.Remove(50);
+
+            //list.RemoveAt(list.Count - 1);
+
+            //if(list.Contains(10))
+            //{
+            //    Console.WriteLine("I found the data");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("I didn't find the data");
+            //}
+
+            //for (int i = 0; i < list.Count; i++)
+            //{
+            //    Console.WriteLine("List [" + i + "] = " + list[i]);
+            //}
+
+            //Console.WriteLine("List Capacity : " + list.Capacity);
+
             #endregion
 
+            #region Stack
+
+            //Stack<int> stack = new Stack<int>();
+
+            //stack.Push(10);
+            //stack.Push(20);
+            //stack.Push(30);
+            //stack.Push(40);
+            //stack.Push(50);
+
+            //int size = stack.Count;
+
+            //for(int i = 0; i < size; i++)
+            //{
+            //    Console.WriteLine(stack.Peek());
+
+            //    stack.Pop();
+            //}
+
+            #endregion
+
+            #region Queue
+
+            Queue<int> queue = new Queue<int>();
+
+            queue.Enqueue(10);
+            queue.Enqueue(20);
+            queue.Enqueue(30);
+            queue.Enqueue(40);
+            queue.Enqueue(50);
+
+
+            if (queue.Contains(60))
+            {
+                Console.WriteLine("I found the data");
+            }
+            else
+            {
+                Console.WriteLine("I didn't find the data");
+            }
+
+            while (queue.Count > 0)
+            {
+                int x = queue.Dequeue();
+
+
+                Console.WriteLine(x);
+            }
+
+            #endregion
+
+            #endregion
         }
 
     }
