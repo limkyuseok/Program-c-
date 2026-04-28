dbc4ae37926f90d88d9474c499065df27d1ce5b0
limkyuseok <lgkk50025@gmail.com>
2026-04-28 21:43:52 +0900

    2026. 04. 28 대리자)

---
+++ Item.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Program
+{
+    internal class Item
+    {
+        public bool Bound;
+        public int Level;
+        public string Name;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,12 +1,29 @@
-﻿using System.Security.Cryptography.X509Certificates;
+﻿using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
 namespace Program
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            #region 열거자
+            // 여러개의 항목으로 이루어진 데이터 집합에서 각 요소를 하나씩
+            // 순서대로 꺼내 사용할 수 있도록 해주는 객체입니다.
 
+            Inventory<Item> inventory = new Inventory<Item>();
+
+            Item knife = new Item();
+
+            knife.Name = "Knife";
+            knife.Level = 15;
+            knife.Bound = true;
+
+            inventory.Add(knife);
+            inventory.Add(knife);
+            inventory.Add(knife);
+            inventory.Add(knife);
+
+            #endregion
         }
-
     }
 }
